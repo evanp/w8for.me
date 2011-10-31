f566b74001417d10bdfef9d820afe7d94c9d7311
Evan Prodromou <evan@status.net>
2011-10-31 16:14:32 -0700

    Move some stuff around

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -16,3 +16,80 @@ window.log = function(){
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+$(function() {
+
+    // Make sure we have global JSON object
+
+    if (!window.JSON) {
+        $.getScript('js/mylibs/json2.js');
+    }
+
+    $.getScript('js/mylibs/uuid.js');
+
+    // Shim for 'number' types
+
+    if (!Modernizr.inputtypes.number) {
+
+        $('input[type="number"]').each(function() {
+            var el = $(this),
+                min = +el.attr('min'),
+                max = +el.attr('max'),
+                step = +el.attr('step'),
+                disabled = el.attr('disabled'),
+                lock = true, // Locking disables lots of key combinations, like Ctrl+V.
+                fr = $('<span class="num-shim" style="position: absolute;">'+
+                       '<span class="up" style="display: block; cursor: pointer;">&#9650;</span>'+
+                       '<span class="down" style="display: block; cursor: pointer;">&#9660;</span></span>');
+
+            // Don't forget to position .num-shim in CSS.
+
+            el.wrap('<span style="position: relative; display: inline-block; overflow: hidden;" />');
+
+            if (disabled) {
+                el.parent().addClass('disabled');
+            }
+
+            el.bind('change', function() {
+                var val = +el.val() || 0;
+                el.val(Math.min(Math.max(min, val), max));
+            });
+
+            el.bind('keydown', function(e) {
+                // ↑ ↓ keys
+                if (e.keyCode === 38) {
+                    valUpdate(step);
+                }
+                if (e.keyCode === 40) {
+                    valUpdate(-step);
+                }
+
+                if (lock) {
+                    // Backspace, enter, escape, delete
+                    if ($.inArray(e.keyCode, [8, 13, 27, 46]) !== -1) return;
+
+                    // Disable input of anything except numbers
+                    if ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
+                        e.preventDefault();
+                    }
+                }
+            });
+
+            fr.insertAfter(el)
+                .find('span')
+                .bind('click', function() {
+                    if ($(this).hasClass('up')) {
+                        valUpdate(step);
+                    } else { 
+                        valUpdate(-step);
+                    }
+                });
+
+            function valUpdate(num) {
+                if (el.attr('disabled')) {
+                    return;
+                }
+                el.val(+el.val() +num).trigger('change');
+            }
+        });
+    }
+});
--- public/js/script.js
+++ public/js/script.js
@@ -197,74 +197,11 @@ $(function() {
 
     $.getScript('js/mylibs/uuid.js');
 
-    // Shim for 'number' types
-
-    if (!Modernizr.inputtypes.number) {
-
-        $('input[type="number"]').each(function() {
-            var el = $(this),
-                min = +el.attr('min'),
-                max = +el.attr('max'),
-                step = +el.attr('step'),
-                disabled = el.attr('disabled'),
-                lock = true, // Locking disables lots of key combinations, like Ctrl+V.
-                fr = $('<span class="num-shim" style="position: absolute;">'+
-                       '<span class="up" style="display: block; cursor: pointer;">&#9650;</span>'+
-                       '<span class="down" style="display: block; cursor: pointer;">&#9660;</span></span>');
-
-            // Don't forget to position .num-shim in CSS.
-
-            el.wrap('<span style="position: relative; display: inline-block; overflow: hidden;" />');
-
-            if (disabled) {
-                el.parent().addClass('disabled');
-            }
-
-            el.bind('change', function() {
-                var val = +el.val() || 0;
-                el.val(Math.min(Math.max(min, val), max));
-            });
-
-            el.bind('keydown', function(e) {
-                // ↑ ↓ keys
-                if (e.keyCode === 38) {
-                    valUpdate(step);
-                }
-                if (e.keyCode === 40) {
-                    valUpdate(-step);
-                }
-
-                if (lock) {
-                    // Backspace, enter, escape, delete
-                    if ($.inArray(e.keyCode, [8, 13, 27, 46]) !== -1) return;
-
-                    // Disable input of anything except numbers
-                    if ((e.keyCode < 48 || e.keyCode > 57) && (e.keyCode < 96 || e.keyCode > 105)) {
-                        e.preventDefault();
-                    }
-                }
-            });
-
-            fr.insertAfter(el)
-                .find('span')
-                .bind('click', function() {
-                    if ($(this).hasClass('up')) {
-                        valUpdate(step);
-                    } else { 
-                        valUpdate(-step);
-                    }
-                });
-
-            function valUpdate(num) {
-                if (el.attr('disabled')) {
-                    return;
-                }
-                el.val(+el.val() +num).trigger('change');
-            }
-        });
-    }
-
     $('form#weight-entry').submit(function(event) {
+
+        // Don't do default processing
+
+        event.preventDefault();
 
         var weight = entryForm.getWeight();
         // zero-indexed
@@ -277,13 +214,13 @@ $(function() {
 
         historyPanel.prependEntry(entry);
         entryForm.showLastEntryDate(entry.ts);
-
-        // Don't do default processing
-
-        return false;
     });
 
     $('form#synch-account').submit(function(event) {
+
+        // Don't do default processing
+
+        event.preventDefault();
 
         var username = $('#username').val();
         var password = $('#password').val();
@@ -294,20 +231,18 @@ $(function() {
 
         entryStore.setAccount(username, password);
 
-        switchTo('synched');
-
-        // Don't do default processing
-
-        return false;
+        UI.switchTo('synched');
     });
 
     $('form#disconnect-account').submit(function(event) {
+        // Don't do default processing
+
+        event.preventDefault();
 
         entryStore.removeAccount();
 
-        switchTo('synch');
+        UI.switchTo('synch');
 
-        return false;
     });
 
     $('#synched').bind('switchto', function (event, data) {
